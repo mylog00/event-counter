b8075a2a6be5b8dd0da6fcf79f6e8180ba6f04f2
mylog00 <mylog00@ya.ru>
2016-10-01 23:19:51 +0500

    Add simple test;

--- src/test/java/com/ym/event_counter/EventCounterTest.java
+++ src/test/java/com/ym/event_counter/EventCounterTest.java
@@ -1,18 +1,42 @@
 package com.ym.event_counter;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for EventCounter.
  */
 public class EventCounterTest {
 
+    private IEventCounter eventCounter;
+
+
+    @Before
+    public void prepare() {
+        eventCounter = new EventCounter();
+    }
+
     /**
-     * Rigourous Test :-)
+     * Simple minute test
      */
     @Test
-    public void testApp() {
-        Assert.assertTrue(true);
+    public void testEventCounterAfterMinute() throws InterruptedException {
+        eventCounter.registerEvent();
+        eventCounter.registerEvent();
+        eventCounter.registerEvent();
+        Assert.assertEquals(3, eventCounter.getEventsPerMinute());
+        Assert.assertEquals(3, eventCounter.getEventsPerHour());
+        Assert.assertEquals(3, eventCounter.getEventsPerDay());
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(61));
+
+        Assert.assertEquals(0, eventCounter.getEventsPerMinute());
+        Assert.assertEquals(3, eventCounter.getEventsPerHour());
+        Assert.assertEquals(3, eventCounter.getEventsPerDay());
     }
+
+
 }
